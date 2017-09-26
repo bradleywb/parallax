27ce45d075b0f77f7acc01bd190dfe6cd2b39f9c
bradleywb <brad.barton@6dglobal.com>
2017-09-26 14:46:30 -0400

    intial commit

---
+++ parallax.js
@@ -0,0 +1,97 @@
+(function (window, $){
+  'use strict';
+
+  var ParallaxScroller = function (element, options){
+    var _ = this;
+    var defaults = {
+
+    };
+
+    _.element = element;
+    _.settings = $.extend({}, defaults, options, true);
+
+    _.SPEED_MAP = Object.freeze({
+      'fast': 80,
+      'normal': 60,
+      'slow': 40
+    });
+
+    return _;
+  };
+
+  ParallaxScroller.prototype.init = function (){
+    var _ = this;
+    var scrollPos;
+
+    _.getLayers();
+
+    // set initial position
+    _.positionLayers();
+
+    $(window).on('scroll', function (evt){
+      // reposition layers
+      _.positionLayers();
+    });
+
+    return _;
+  };
+
+  ParallaxScroller.prototype.getLayers = function (){
+    var _ = this;
+    var layers = $(_.element).find('[data-parallax-layer]');
+
+    _.layers = [];
+
+    layers.each(function (i, layer){
+      _.layers[i] = {
+        element: layer,
+        speed: _.getLayerSpeed(layer)
+      };
+      _.getLayerSpeed(layer);
+    });
+
+    return _;
+  };
+
+  ParallaxScroller.prototype.getLayerSpeed = function (layer){
+    var _ = this;
+    var speedSetting = $(layer).data('speed') || $(layer).attr('[data-speed]');
+    var speed;
+
+    switch(speedSetting){
+    case 'fast':
+    case 'normal':
+    case 'slow':
+      speed = _.SPEED_MAP[speedSetting];
+    break;
+    default:
+      speed = speedSetting;
+    }
+
+    return speed;
+  };
+
+  ParallaxScroller.prototype.positionLayers = function (){
+    var _ = this;
+    var scrollPos = _.element.getBoundingClientRect().top;
+
+    $(_.layers).each(function (i, layer){
+      $(layer.element).css({
+        'top': (scrollPos * (layer.speed / 100)) + 'px'
+      });
+    });
+
+    return _;
+  };
+
+  // jQuery Plugin
+  $.fn.parallaxScroller = function (options){
+    return this.each(function (i, element){
+      return new ParallaxScroller(element, options).init();
+    });
+  };
+
+  $(document).ready(function (){
+    $('[data-parallax]').parallaxScroller();
+  });
+})(window, jQuery);
